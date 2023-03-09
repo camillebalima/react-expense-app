61cdb20c27151ba8086e055a4a9d7c750cfacf35
Camille Balima <camille.balima@pinnacle-oa.com>
2023-03-09 13:52:37 -0400

    Training Done

--- 01-starting-setup/src/components/Chart/Chart.js
+++ 01-starting-setup/src/components/Chart/Chart.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import ChartBar from './ChartBar'
-import './Chart.css'
+import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css";
 
 const Chart = (props) => {
-  return (
-    <div className='chart'>
-        {props.dataPoints.map(dataPoint => <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>)}
-    </div>
-  )
-}
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximun = Math.max(...dataPointValues);
 
-export default Chart
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximun}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import './Expenses.css'
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,6 +18,7 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
